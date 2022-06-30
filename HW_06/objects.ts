8416d80470b78868d22550df6f7e8a7b68431fba
Milana <milanin7147@gmail.com>
2022-06-30 16:12:47 +0400

    some changes

---
+++ HW_06/objects.ts
@@ -0,0 +1,22 @@
+export const expectedObjGet: {
+
+    id: number,
+    email: string,
+    first_name: string,
+    last_name: string,
+    avatar: string
+} = {
+    id: 1,
+    email: 'george.bluth@reqres.in',
+    first_name: 'George',
+    last_name: 'Bluth',
+    avatar: 'https://reqres.in/img/faces/1-image.jpg'
+};
+
+export const expectedObjPost: {
+    name: string,
+    role: string,
+} = {
+    name: 'Milana',
+    role: 'Student',
+};
--- HW_06/test1.spec.ts
+++ HW_06/test1.spec.ts
@@ -1,29 +1,6 @@
 import superagent from "superagent";
-
-
-const expectedObjGet: {
-
-    id: number,
-    email: string,
-    first_name: string,
-    last_name: string,
-    avatar: string
-} = {
-    id: 1,
-    email: 'george.bluth@reqres.in',
-    first_name: 'George',
-    last_name: 'Bluth',
-    avatar: 'https://reqres.in/img/faces/1-image.jpg'
-};
-
-const expectedObjPost: {
-    name: string,
-    role: string,
-} = {
-    name: 'Milana',
-    role: 'Student',
-};
-
+import {expectedObjPost} from "./objects";
+import {expectedObjGet} from "./objects";
 
 describe("HTTP requests", () => {
     test("Check http request: post: status", async () => {
